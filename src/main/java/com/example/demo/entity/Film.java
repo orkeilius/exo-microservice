b5e1756be80bc26f22f74c3da7cb90698366c2eb
orkeilius <63045634+orkeilius@users.noreply.github.com>
2026-02-12 08:28:13 +0100

    tech []: add images

--- src/main/java/com/example/demo/entity/Film.java
+++ src/main/java/com/example/demo/entity/Film.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,9 +23,19 @@ public class Film {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 255)
     private String nom;
+
+    @Column(length = 255)
     private String genre;
+
     private Duration duration;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
+
+    @Lob
+    @Column(columnDefinition = "CLOB")
     private String imageData;
 }
